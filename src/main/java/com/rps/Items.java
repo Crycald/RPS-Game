a86eed48b37103cbcb0a1e734db21fd70039b549
Crycald <krzysiekdybikowski@gmail.com>
2020-05-07 09:41:35 +0200

    added items

---
+++ src/main/java/com/rps/Items.java
@@ -0,0 +1,19 @@
+package com.rps;
+
+public class Items {
+    private final static String SCISSORS = "SCISSORS";
+    private final static String ROCK = "ROCK";
+    private final static String PAPER = "PAPER";
+
+    public static String getSCISSORS() {
+        return SCISSORS;
+    }
+
+    public static String getROCK() {
+        return ROCK;
+    }
+
+    public static String getPAPER() {
+        return PAPER;
+    }
+}
