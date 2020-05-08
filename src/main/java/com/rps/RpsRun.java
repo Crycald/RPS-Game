0dc90ea4b2f4e020d09cd17cfee20a652fa72b8d
Crycald <krzysiekdybikowski@gmail.com>
2020-05-08 19:25:01 +0200

    backup

--- src/main/java/com/rps/RpsRun.java
+++ src/main/java/com/rps/RpsRun.java
@@ -6,6 +6,8 @@ import com.rps.logic.Game;
 public class RpsRun {
     public static void main(String[] args) {
         Game game = new Game();
-        game.start();
+        game.consoleInfoNick();
+        game.consoleInfoRounds();
+
     }
 }
--- src/main/java/com/rps/StartGame.java
+++ src/main/java/com/rps/StartGame.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 public class StartGame {
     private String nick;
     private final String aiNick = "Computer";
-    private int winRounds;
+    private int winRounds = 1;
     private Path root = FileSystems.getDefault().getPath("").toAbsolutePath();
 
     public void setNick(String nick) {
